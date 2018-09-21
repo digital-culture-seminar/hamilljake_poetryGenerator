824b0dfb4111798ecc49f219e2531ee35f21cc7e
jakehamill <jhami49@lsu.edu>
2018-09-21 18:28:46 -0500

    from paragraphs to general ideas (bits of sentences)

--- generator.py
+++ generator.py
@@ -73,18 +73,11 @@ import random
 
 # list of nouns
 
-poems = ["The storm bubbles on the horizon. It's always a transformation. "
-         "It's always a transformation and a distant transformation. "
-         "Light is poured out through holes in the thick grey clouds. "
-         "The twinkling of a hard house. The generation of bright colored paint. "
-         "Paint maintaining this structure burnout with bloated bones. " 
-         "If we paint the house one more time then I may look at it once again. "
-         "This grey wall is standing well.", "Storms are blowing on the horizon. "
-         "Shapeshifting is a generation of bright colored paint that is always farther and far away, "
-         "and is the shaking house in which light is poured into the hole in a thick grey cloud. "
-         "I'm tired of the painted bone that holds these structures, and I'm tired of these houses, "
-         "but I'm going to see them again, and I'm standing on this grey wall, "
-         "and I'm standing on this grey wall. "]
+line1 = ["The storm bubbles on the horizon. ", "Storms are blowing on the horizon. ",
+         "The storm bubbles far on the horizon. "]
+
+line2 = ["Shapeshifting, always moving closer and farther away. ", " Its always a transformation "
+         "and a distant transformation. ", "Shapeshifting is always farther and far away. "]
 
 
 # list of verbs
@@ -103,11 +96,11 @@ poems = ["The storm bubbles on the horizon. It's always a transformation. "
 
 # random + new variable
 
-poem = random.choice(poems)
-#verb = random.choice(verbs)
+first = random.choice(line1)
+second = random.choice(line2)
 #adjective = random.choice(adjectives)
 
-print poem
+print first + second
 
 #i = 0
 #for verb in verbs:
